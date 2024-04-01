643b88b0d9fdf033544f4e345577c4c0e5afa37a
osparking <jbpark03@gmail.com>
2024-04-02 08:29:54 +0900

    두 생성자 추가

--- src/main/java/space/bum/spring_boot/error/BookNotFoundException.java
+++ src/main/java/space/bum/spring_boot/error/BookNotFoundException.java
@@ -1,5 +1,16 @@
 package space.bum.spring_boot.error;
 
-public class BookNotFoundException extends Exception{
+public class BookNotFoundException extends Exception {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
+  public BookNotFoundException() {
+    super();
+  }
+
+  public BookNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
