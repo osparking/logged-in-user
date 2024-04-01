04503e42913ff3b670dfdb206d3e334636ce5eea
osparking <jbpark03@gmail.com>
2024-04-01 22:08:44 +0900

    저장소 및 영역 클래스 스캔 패키지

--- src/main/java/space/bum/spring_boot/Application.java
+++ src/main/java/space/bum/spring_boot/Application.java
@@ -2,7 +2,11 @@ package space.bum.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("space.bum.spring_boot.repository") 
+@EntityScan("space.bum.spring_boot.domain")
 @SpringBootApplication
 public class Application {
 
