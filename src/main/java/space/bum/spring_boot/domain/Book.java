53e82eec46c3ba8575100e9b4f0027d147f2a730
osparking <jbpark03@gmail.com>
2024-04-02 07:24:19 +0900

    book ID getter

--- src/main/java/space/bum/spring_boot/domain/Book.java
+++ src/main/java/space/bum/spring_boot/domain/Book.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Book {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,5 +19,8 @@ public class Book {
   
   @Column(nullable = false) 
   private String author;
-  
+
+  public long getId() {
+    return id;
+  }
 }
