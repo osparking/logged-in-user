73cdaad19febdec7780d4027075ee0bb9e1daf45
osparking <jbpark03@gmail.com>
2024-04-02 12:13:17 +0900

    제목으로 책 검색 - 성공

--- src/test/java/space/bum/spring_boot/SpringBootBootstrapLiveTest.java
+++ src/test/java/space/bum/spring_boot/SpringBootBootstrapLiveTest.java
@@ -2,6 +2,9 @@ package space.bum.spring_boot;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,17 @@ import io.restassured.response.Response;
 import space.bum.spring_boot.domain.Book;
 
 public class SpringBootBootstrapLiveTest {
+  @Test
+  public void whenGetBooksByTitle_thenOK() {
+    Book book = createRandomBook();
+    createBookAsUri(book);
+    Response response = RestAssured.get(
+        API_ROOT + "/title/" + book.getTitle());
+
+    assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    assertTrue(response.as(List.class)
+        .size() > 0);
+  }
 
   @Test
   public void whenGetAllBooks_thenOK() {
