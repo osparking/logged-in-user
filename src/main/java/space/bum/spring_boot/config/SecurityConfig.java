cdd79b1c480bdc2a138f86fbe3eaab4bc6e6c581
osparking <jbpark03@gmail.com>
2024-04-02 15:41:00 +0900

    보안 방언 용도 시범용 두 사용자

--- src/main/java/space/bum/spring_boot/config/SecurityConfig.java
+++ src/main/java/space/bum/spring_boot/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package space.bum.spring_boot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +22,22 @@ public class SecurityConfig {
             .permitAll())
         .csrf(AbstractHttpConfigurer::disable);
     return http.build();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth)
+      throws Exception {
+    auth
+        .inMemoryAuthentication()
+        .withUser("user").password(passwordEncoder().encode("user"))
+        .roles("USER")
+        .and()
+        .withUser("admin").password(passwordEncoder().encode("admin"))
+        .roles("ADMIN");
+  }
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
